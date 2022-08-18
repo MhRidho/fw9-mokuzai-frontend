fb66a69f717731bad82e03f49cab7f914625c77b
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-08-18 10:10:50 +0700

    feat: auth template

---
+++ components/organisms/MainTemplateAuth/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+import { Container } from 'react-bootstrap'
+import ComHeader from '../ComHeader'
+import ComFooter from '../ComFooter'
+import ComBannerAuth from '../ComBannerAuth'
+
+
+export default function MainTemplateAuth(props) {
+  return (
+    <>
+      <Head>
+        <title>{props.title ? props.title : 'MOKUZAI'}</title>
+      </Head>
+      <ComHeader />
+      <ComBannerAuth title={props.titleBanner} desc={props.descBanner} />
+      <Container>
+        {props.children}
+      </Container>
+      <ComFooter />
+    </>
+  )
+}
